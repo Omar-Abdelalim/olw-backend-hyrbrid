d7ed7a7bf3dbc7e563a104f1f43bdf2d00cdabf1
Omar-Abdelalim <omar@onelinkwallet.com>
2024-08-13 17:08:50 +0300

    middleware

--- apis/version2/transactions.py
+++ apis/version2/transactions.py
@@ -865,7 +865,7 @@ def calcFee(db,amount,serviceCode,merchantID:None):
     fee = db.query(Fee).filter(Fee.serviceCode==sCode,Fee.merchantID == mID,Fee.status == "active").first()
     if fee is None:
         return {"status_code":401,"message":"no fee exists with this code"}
-    print(fee)
+    print(fee.feeFixed,fee.feeRate)
     print(amount)
     print((fee.feeFixed+fee.feeRate/100*float(amount)))
     feeAmount = (1+fee.campaign)*(fee.feeFixed+fee.feeRate/100*float(amount))
