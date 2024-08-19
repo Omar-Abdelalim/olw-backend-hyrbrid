746b5a258e8119cf41f36e3d14aaf33f8928201e
Omar-Abdelalim <omar@onelinkwallet.com>
2024-08-19 18:31:58 +0300

    error trapping

--- apis/version2/transactions.py
+++ apis/version2/transactions.py
@@ -262,77 +262,77 @@ async def tansaction1(request: Request,response: Response,payload: dict = Body(.
 
 @router.post("/transactionMerchant")
 async def tansaction1(request: Request,response: Response,payload: dict = Body(...),db: Session = Depends(get_db)):
-        try:
-            payload = await request.body()
-            # payload = json.loads(payload)
-            # payload = payload['message']
-            payload = json.loads(payload)
-            token = payload['token']
+        # try:
+        payload = await request.body()
+        # payload = json.loads(payload)
+        # payload = payload['message']
+        payload = json.loads(payload)
+        token = payload['token']
+
+    
+        print('payload:',payload)
+        qrt = db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "pending").first()
+        if qrt is None:
+            return{"status_code":403,"message":"terminal qr code is not pending here"}
+        db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "pending").update({"qrStatus":"processing"})
+        db.commit()
+    
+        
+        
+        OLWBank = db.query(Account).filter(Account.accountNumber == "10-00000003-001-000").first()
+
+
+        sendCus = db.query(Customer).filter(Customer.id==payload["id"]).first()
+
+        if not sendCus.pin == payload["pin"]:
+            return {"status_code": 401,"message":"Pin incorrect"}
+
+        sendAcc = db.query(Account).filter(Account.accountNumber==payload["fromAccount"]).first()
+        if sendAcc is None:
+            return {"status_code": 401,"message":"return account doesn't exist"}
+        if payload["amount"] > sendAcc.balance:
+            return {"status_code": 401,"message":"balance not enough"}
+
+        if OLWBank is None:
+            return{"status_code":404,"message":"please make sure olw bank account are intialized"}
+        qrt = db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "processing").first()
+        if qrt is None:
+            return{"status_code":403,"message":"terminal qr code is not pending here"}
+        trans = transactionOperation(payload["fromAccount"],qrt.terminalID,qrt.amount,payload["fromCurrency"],payload["toCurrency"],db,displayName="merchant:"+qrt.merchantName,merchantAccount = qrt.merchantAccount)
+        if not trans["status_code"]==201:
+            return trans
+        r =requests.get("http://192.223.11.185:8080/terminal", json={'id': payload["terminalID"]})
+        print(r)
+        r = json.loads(r.content)
+        print("response:",r)
+
+        fee = calcFee(db,payload["amount"],"MR002",r["merchantID"])
+        print(fee)
+        if fee>0:
+            trans2 = transactionOperation(qrt.merchantAccount,"10-00000005-001-000",payload["fees"],payload["fromCurrency"],payload["toCurrency"],db)
+            
+            if not trans2["status_code"]==201:
+                return trans2
+    
+
+        # q = db.query(QR).filter(QR.id == payload["qrID"]).first()
+        
+        # if q is None:
+        #     return{"status_code":404,"message":"no qr code exists with this ID"}
+        # elif q.qrStatus == "completed":
+        #     return{"status_code":404,"message":"transaction already complete"}
+
 
         
-            print('payload:',payload)
-            qrt = db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "pending").first()
-            if qrt is None:
-                return{"status_code":403,"message":"terminal qr code is not pending here"}
-            db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "pending").update({"qrStatus":"processing"})
-            db.commit()
-        
-            
-            
-            OLWBank = db.query(Account).filter(Account.accountNumber == "10-00000003-001-000").first()
+        db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "processing").update({"transactionID":trans["t1"]})
+
+        db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "processing").update({"qrStatus":"completed"})
 
 
-            sendCus = db.query(Customer).filter(Customer.id==payload["id"]).first()
-
-            if not sendCus.pin == payload["pin"]:
-                return {"status_code": 401,"message":"Pin incorrect"}
-
-            sendAcc = db.query(Account).filter(Account.accountNumber==payload["fromAccount"]).first()
-            if sendAcc is None:
-                return {"status_code": 401,"message":"return account doesn't exist"}
-            if payload["amount"] > sendAcc.balance:
-                return {"status_code": 401,"message":"balance not enough"}
-
-            if OLWBank is None:
-                return{"status_code":404,"message":"please make sure olw bank account are intialized"}
-            qrt = db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "processing").first()
-            if qrt is None:
-                return{"status_code":403,"message":"terminal qr code is not pending here"}
-            trans = transactionOperation(payload["fromAccount"],qrt.terminalID,qrt.amount,payload["fromCurrency"],payload["toCurrency"],db,displayName="merchant:"+qrt.merchantName,merchantAccount = qrt.merchantAccount)
-            if not trans["status_code"]==201:
-                return trans
-            r =requests.get("http://192.223.11.185:8080/terminal", json={'id': payload["terminalID"]})
-            print(r)
-            r = json.loads(r.content)
-            print("response:",r)
-
-            fee = calcFee(db,payload["amount"],"MR002",r["merchantID"])
-            print(fee)
-            if fee>0:
-                trans2 = transactionOperation(qrt.merchantAccount,"10-00000005-001-000",payload["fees"],payload["fromCurrency"],payload["toCurrency"],db)
-                
-                if not trans2["status_code"]==201:
-                    return trans2
-        
-
-            # q = db.query(QR).filter(QR.id == payload["qrID"]).first()
-            
-            # if q is None:
-            #     return{"status_code":404,"message":"no qr code exists with this ID"}
-            # elif q.qrStatus == "completed":
-            #     return{"status_code":404,"message":"transaction already complete"}
-
-
-            
-            db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "processing").update({"transactionID":trans["t1"]})
-
-            db.query(QRTer).filter(QRTer.terminalID == payload["terminal"],QRTer.qrStatus == "processing").update({"qrStatus":"completed"})
-
-
-        except Exception as e:
-            message = "exception occurred with creating transaction"
-            log(0,message)
-            return {"status_code":401,"message":e}
+        # except Exception as e:
+        #     message = "exception occurred with creating transaction"
+        #     log(0,message)
+        #     return {"status_code":401,"message":e}
         log(1,"from:{}, to:{}, amount:{},sending currency:{}, receiving currency:{}".format(payload["fromAccount"],payload["terminal"],payload["amount"],payload["fromCurrency"],payload["toCurrency"]))
         db.commit()
         db.refresh(sendAcc)
