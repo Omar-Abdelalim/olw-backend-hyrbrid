ff973bf9ec14214e0bf51161a30bbb0b16cb2681
Omar-Abdelalim <omar@onelinkwallet.com>
2024-08-27 16:54:30 +0300

    transaction type

--- apis/version2/transactions.py
+++ apis/version2/transactions.py
@@ -927,7 +927,7 @@ def generateTranIdentifier(db,tcode):
 
     print("a")
     tt = db.query(TransactionType).first()
-    return tt
+    return {"status_code":401,"a":tt}
     t = db.query(TransactionType).filter(TransactionType.code == tcode,TransactionType.status == "active").first()
     print(t)
     if t is None:
